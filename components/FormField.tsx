25fe801ed70b3116b5d7f6ee7953f6512ada96e4
Justin <devdesignjustin@gmail.com>
2025-08-17 16:44:33 -0400

    make auth pages

---
+++ components/FormField.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { Controller, Control, FieldValues, Path } from "react-hook-form";
+
+import {
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+interface formFieldProps<T extends FieldValues> {
+  control: Control<T>;
+  name: Path<T>;
+  label: string;
+  placeholder?: string;
+  type?: "text" | "email" | "password" | "file";
+}
+
+const FormField = ({
+  control,
+  name,
+  label,
+  placeholder,
+  type = "text",
+}: formFieldProps<T>) => (
+  <Controller
+    name={name}
+    control={control}
+    render={({ field }) => (
+      <FormItem>
+        <FormLabel className="label">{label}</FormLabel>
+        <FormControl>
+          <Input className="input" placeholder={placeholder} type={type} {...field} />
+        </FormControl>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+);
+
+export default FormField;
