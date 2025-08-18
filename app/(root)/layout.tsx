6b394be285c34736b69766db2d857ac2a3f2fdd7
Justin <devdesignjustin@gmail.com>
2025-08-18 16:07:16 -0400

    develop homepage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,19 @@
-import { ReactNode } from 'react'
+import { ReactNode } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-const RootLayout = ({children}: {children: React.ReactNode}) => {
-    return (
-        <div>
-        {children}
-        </div>
-    )
-}
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="Logo" width={32} height={38} />
+          <h2 className="text-primary-100">PrepWise</h2>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
+};
 
-export default RootLayout
+export default RootLayout;
